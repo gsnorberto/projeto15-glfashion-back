3d8c37a30f7d98d1bb21bcb3edaa95cd19b58ddc
Laura <laurasouzacastro1@gmail.com>
2023-01-25 13:44:40 -0300

    feat: CRUD da collection purchases

--- src/controllers/purchaseController.js
+++ src/controllers/purchaseController.js
@@ -1,11 +1,53 @@
 import db from "../config/database.js"
-
+import { ObjectId } from "mongodb";
 
 export async function addPurchase(req, res) {
-    
+    try {
+        const { idProduct, name, url, size, quantity } = req.body;
+        //const { idUsuario } = ;
+        const idUsuario = '1' //TEMPORARY
+
+        const updateProduct = await db.collection("purchases").findOne({idProduct, size, idUsuario})
+        
+        if(updateProduct) {
+            await db.collection("purchases").updateOne({ idProduct, size, idUsuario}, { $set: {quantity: parseInt(updateProduct.quantity) + parseInt(quantity) } });
+            return res.sendStatus(201);
+        }
+        
+        await db.collection("purchases").insertOne({ idProduct, name, url, size, quantity, idUsuario});
+        res.sendStatus(201);
+    } catch (error) {
+      res.status(500).send("Erro no servidor!");
+    }
 }
 
 export async function getPurchases(req, res) {
-
+    try {
+        //const { idUsuario } = ;
+        const idUsuario = '1' //TEMPORARY
+        const purchases = await db.collection("purchases").find({idUsuario}).toArray();
+        res.send(purchases);
+    } catch (error) {
+        res.status(500).send("Erro no servidor");
+    }
 }
 
+export async function putPurchases(req, res) {
+    try {
+        const { idPurchase, quantity } = req.body;
+        await db.collection("purchases").updateOne({ _id: ObjectId(idPurchase) }, { $set: {quantity} });
+            return res.sendStatus(204);
+    } catch (error) {
+        res.status(500).send("Erro no servidor");
+    }
+}
+
+export async function deletePurchases(req, res) {
+    try {
+        const { idPurchase } = req.body;
+        await db.collection("purchases").deleteOne({_id: ObjectId(idPurchase)});
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).send("Erro no servidor");
+    }
+}
--- src/routes/purchaseRoutes.js
+++ src/routes/purchaseRoutes.js
@@ -1,11 +1,18 @@
 import express from 'express'
+import { addPurchase, getPurchases, putPurchases, deletePurchases } from '../controllers/purchaseController.js'
+import { validateSchema } from "../middlewares/validateSchema.js"
+import { addPurchaseSchema, putPurchaseSchema, deletePurchaseSchema } from '../schemas/purchase.schema.js'
+
 let purchaseRouter = express.Router()
-import { addPurchase, getPurchases } from '../controllers/purchaseController.js'
 
 // add purchase created by user
-purchaseRouter.post('/purchase', addPurchase)
+purchaseRouter.post('/purchases', validateSchema(addPurchaseSchema), addPurchase)
 
 // get all user purchases
-purchaseRouter.post('/all-purchases', getPurchases)
+purchaseRouter.get('/purchases', getPurchases)
+
+purchaseRouter.put('/purchases', validateSchema(putPurchaseSchema), putPurchases)
+
+purchaseRouter.delete('/purchases', validateSchema(deletePurchaseSchema), deletePurchases)
 
 export default purchaseRouter
