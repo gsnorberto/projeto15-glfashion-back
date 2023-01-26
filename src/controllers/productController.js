ebd9637ef81f7a7137520c98ee6806ae97707dff
FabioGarroteDev <fgarrote12@gmail.com>
2023-01-26 00:41:16 -0300

    feat: adicionada a função de mandar os produtos para o front pelo get

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,17 @@
-import db from "../config/database.js"
-
+import db from '../config/database.js';
 
 export async function addProduct(req, res) {
-    
+
 }
 
 export async function getProducts(req, res) {
+  try {
+    const products = await db.collection('products').find().toArray();
 
+	  res.send(products);
+  } catch (err) {
+    res.status(500).send('Erro no sevidor')
+  }
 }
 
 export async function getProduct(req, res) {
