dbf1d0537ff219b3bd7255ec600fe2eecf2f6961
Laura <laurasouzacastro1@gmail.com>
2023-01-30 17:38:03 -0300

    fix: cors

--- src/app.js
+++ src/app.js
@@ -6,14 +6,11 @@ import productRouter from './routes/productRoutes.js'
 import authRouter from './routes/authRoutes.js'
 import ordersRouter from './routes/checkoutRoutes.js'
 dotenv.config()
-
+const corsOptions = {
+  origin: '*'
+}
 const app = express()
-app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }))
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
