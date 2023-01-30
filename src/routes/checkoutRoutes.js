086b833c0fc2fa218d13f120be247eebf2200228
Laura <laurasouzacastro1@gmail.com>
2023-01-30 12:49:58 -0300

    feat: get dos pedidos

--- src/routes/checkoutRoutes.js
+++ src/routes/checkoutRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { addOrders } from '../controllers/checkoutController.js'
+import { addOrders, getOrders } from '../controllers/checkoutController.js'
 import { validateSchema } from "../middlewares/validateSchema.js"
 import { addOrderSchema } from '../schemas/checkout.schema.js'
 
 let ordersRouter = express.Router()
 
-ordersRouter.post('/pagamento', validateSchema(addOrderSchema), addOrders)
+ordersRouter.post('/orders', validateSchema(addOrderSchema), addOrders)
+
+ordersRouter.get('/orders/:idUsuario', getOrders)
 
 export default ordersRouter
